d318d06a3a324f6a8b47fe18bf420ad6ebd8aae7
gustavogomez <gomezf09@gmail.com>
2024-09-26 10:34:50 -0500

    Install NextUI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,5 @@
-import type { Metadata } from "next";
-// import localFont from "next/font/local";
+import type {Metadata} from "next";
 import "./globals.css";
-
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,17 +7,15 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                     children,
+                                   }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="es">
+    <body>
+     {children}
+    </body>
     </html>
   );
 }
